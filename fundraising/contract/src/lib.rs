33325006cae51e33f502c2b42f46bbe430ae1d41
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2022-12-02 08:29:13 +0200

    Added fundraising contract.

---
+++ fundraising/contract/src/lib.rs
@@ -0,0 +1,22 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::{log, near_bindgen, AccountId};
+use near_sdk::collections::{UnorderedMap};
+
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    pub fundraiser: AccountId,
+    pub pledges: UnorderedMap<AccountId, u128>
+}
+
+impl Default for Contract{
+    fn default() -> Self{
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+
+}
