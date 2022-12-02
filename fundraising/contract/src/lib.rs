dc2b16eb1d829caeda05812cad53d3dacac7a98f
vadim.nicolai-EXT <vadim.nicolai-EXT@vitrifi.net>
2022-12-02 08:49:01 +0200

    get_fundraiser

--- fundraising/contract/src/lib.rs
+++ fundraising/contract/src/lib.rs
@@ -1,7 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
-use near_sdk::{log, near_bindgen, AccountId};
-use near_sdk::collections::{UnorderedMap};
+use near_sdk::{env, near_bindgen, AccountId};
 
 
 #[near_bindgen]
@@ -22,5 +21,18 @@ impl Default for Contract{
 
 #[near_bindgen]
 impl Contract {
+    #[init]
+    #[private]
+    pub fn init(fundraiser: AccountId) -> Self {
+        assert!(!env::state_exists(), "Contract already initialized");
 
+        Self {
+            fundraiser: fundraiser,
+            pledges: UnorderedMap::new(b"p")
+        }
+    }
+
+    pub fn get_fundraiser(&self) -> AccountId {
+        self.fundraiser.clone()
+    }
 }
